3fc4f9ae2898da9fab0823dcbd77d0b971e5ce77
Stefano Locati <stefano@obliquid.it>
2016-02-03 14:36:58 +0000

    fixed package scanning config

--- src/main/java/stivlo/house/config/AppConfig.java
+++ src/main/java/stivlo/house/config/AppConfig.java
@@ -5,6 +5,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = {"house.stivlo"})
+@ComponentScan(basePackages = {"stivlo.house"})
 public class AppConfig {
 }
